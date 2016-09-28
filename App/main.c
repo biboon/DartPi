f7c4f6bfd17d371dcc887db272025f4cdc5097b0
romain <r.libaert@gmail.com>
2016-09-28 18:11:46 +0200

    Should be more efficient

--- App/main.c
+++ App/main.c
@@ -2,70 +2,108 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
+#include <string.h>
 #include <sys/time.h>
 #include <sys/types.h>
 
 #include <libthrd.h>
 
-unsigned long* hits = NULL;
+
+typedef struct {
+	pthread_t tid;
+	int id;
+	unsigned long shot, hit, step, *remaining;
+	int *semid;
+} WorkerInfo;
+
+
 
 void loop(void* params) {
-	unsigned long* _params = (unsigned long*) params;
+	WorkerInfo *worker = (WorkerInfo *) params;
+	unsigned long todo;
 	struct drand48_data buffer;
 	double x, y;
-	srand48_r(time(NULL) * _params[1], &buffer);
-	for( ; _params[0] > 0; _params[0]--) {
-		drand48_r(&buffer, &x);
-		drand48_r(&buffer, &y);
-		if ((x*x + y*y) < 1) hits[_params[1]]++;
+	srand48_r(time(NULL) * (1 + worker->id), &buffer);
+
+	P(*(worker->semid), 0);
+	while (*(worker->remaining)) {
+		todo = (*(worker->remaining) >= worker->step) ? worker->step : *(worker->remaining);
+		*(worker->remaining) -= todo;
+		V(*(worker->semid), 0);
+		worker->shot += todo;
+		for ( ; todo > 0; --todo) {
+			drand48_r(&buffer, &x);
+			drand48_r(&buffer, &y);
+			if ((x*x + y*y) < 1) (worker->hit)++;
+		}
+		P(*(worker->semid), 0);
 	}
+	V(*(worker->semid), 0);
+
+	#ifdef DEBUG
+		printf("%d: %ld shots for %ld hits\n", worker->id, worker->shot, worker->hit);
+	#endif
 }
 
 
 /** Main function **/
 int main(int argc, char** argv) {
-	unsigned long shots = 0, threads = 0, tothits = 0, params[2], i = 0;
+	unsigned long remaining, threads, step, i, tothits = 0, totshots = 0;
+	int semid;
 	clock_t start, end;
 	struct timeval tstart, tend;
 
+	/* Printing some info */
+	printf("This sofware will give an approximation of Pi using Dartboard algorithm\n");
 	if (argc < 3) {
 		printf("Usage: %s shots threads\n", argv[0]);
 		return -1;
 	}
-	shots = strtol(argv[1], NULL, 10);
+
+	/* Initialization */
+	remaining = strtol(argv[1], NULL, 10);
 	threads = strtol(argv[2], NULL, 10);
-
-	pthread_t tids[threads - 1];
-	hits = (unsigned long*) calloc(threads, sizeof(unsigned long));
-	params[0] = shots / threads;
-	shots = params[0] * threads;
-
-	printf("This sofware will give an approximation of Pi using\n");
-	printf("Dartboard algorithm with %ld shots and %ld threads\n", shots, threads);
+	step = remaining / threads / 20;
+	WorkerInfo workers[threads];
+	memset(workers, 0, threads * sizeof(WorkerInfo));
+	semid = initMutexes(1, 1);
+	printf("Starting with %ld shots and %ld threads\n", remaining, threads);
 
 	start = clock();
 	gettimeofday(&tstart, NULL);
 
+	/* Start all the workers */
 	for (i = 1; i < threads; i++) {
-		params[1] = i;
-		startThread(tids + i - 1, loop, (void*)params, 2 * sizeof(unsigned long));
+		workers[i].id = i;
+		workers[i].remaining = &remaining;
+		workers[i].step = step;
+		workers[i].semid = &semid;
+		startThread(&workers[i].tid, loop, workers + i, sizeof(WorkerInfo));
 	}
 
-	params[1] = 0;
-	loop(params);
+	/* Start the work for the main thread */
+	//workers[0].id = 0; // Should not be necessary since we used memset
+	workers[0].remaining = &remaining;
+	workers[0].step = step;
+	workers[0].semid = &semid;
+	loop(workers);
 
-	// Wait until each thread returns
-	for (i = 1; i < threads; i++) waitThread(tids[i - 1]);
+	/* Wait until each thread returns */
+	for (i = 1; i < threads; i++) waitThread(workers[i].tid);
 
-	for (i = 0; i < threads; i++) tothits += hits[i];
-	long double pi = (long double)(tothits << 2)/(long double)shots;
+	/* See results */
+	for (i = 0; i < threads; i++) {
+		tothits += workers[i].hit;
+		totshots += workers[i].shot;
+	}
+	long double pi = (long double)(tothits * 4)/(long double)totshots;
 	end = clock();
 	gettimeofday(&tend, NULL);
 
+	/* Display results */
 	printf("Pi is %.12Lf\n\n", pi);
 	printf("CPU time: %f s\n", (double)(end - start)/CLOCKS_PER_SEC);
 	printf("Real time: %f s\n", (double)(tend.tv_usec - tstart.tv_usec)/1e6 + (double)(tend.tv_sec - tstart.tv_sec));
-	free(hits);
 
 	return 0;
 }
